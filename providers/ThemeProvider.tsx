ce1b3dc054cf5324514b2fa53330b4ef0d1bf59d
Husni Kamal <husnikamal3@gmail.com>
2024-06-04 18:59:56 +0700

    fix(theme): add default theme

--- providers/ThemeProvider.tsx
+++ providers/ThemeProvider.tsx
@@ -2,7 +2,11 @@ import { ThemeProvider } from "next-themes";
 import { FC, PropsWithChildren } from "react";
 
 const Theme: FC<PropsWithChildren> = ({ children }) => {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="light">
+      {children}
+    </ThemeProvider>
+  );
 };
 
 export default Theme;
